c1cd1af456ee93becec7cd942da979a3275267ee
RenanFStein <renan_stein@hotmail.com>
2022-09-07 18:52:32 -0300

    Second Commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,5 +7,46 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
         System.out.println("Renan");
+
+        String nome;
+        int numPaginas;
+        Livro livro1 = new Livro(nome="Livro Teste", numPaginas= 150);
+        System.out.println(livro1);
+
+
+    }
+
+}
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
